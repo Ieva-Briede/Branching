304150b60b070b1ee9cc4ec66018966ee0ae162d
Ieva-Briede <briede.ieva.1@gmail.com>
2021-02-15 11:24:03 +0200

    Add assigment 7

--- src/com/assigment/AddArrAssigment7.java
+++ src/com/assigment/AddArrAssigment7.java
@@ -1,4 +1,24 @@
 package com.assigment;
 
+
 public class AddArrAssigment7 {
+
+    public static void main(String[] args) {
+        System.out.println("Is your choosed number palindrome? \n" + isPalindrome(11212));
+        //-1221
+        //707
+        //11212)
+        }
+
+        public static boolean isPalindrome(int number){
+        int originalNumber = number;
+        int reversedNumber = 0;
+        while (originalNumber != 0) {
+           int reminder = originalNumber % 10;
+            reversedNumber = reversedNumber * 10 + reminder;
+            originalNumber = originalNumber / 10;
+        }
+
+        return number == reversedNumber;
+    }
 }
